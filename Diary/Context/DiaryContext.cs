a6bb660b2043e0be4f9830bc8d859ced2f0092c4
Monika Nogała <mxnogala@gmail.com>
2021-06-09 11:02:21 +0200

    create model and database connection

---
+++ Diary/Context/DiaryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Diary.Models;
+
+namespace Diary.Controllers
+{
+    public class DiaryContext : DbContext
+    {
+            public DbSet<DiaryModel> DiaryEntry { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseSqlite(@"Data Source=diary.db");
+
+    }
+}
---
+++ Diary/Models/Diary.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diary.Models
+{
+    public class DiaryModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Entry { get; set; }
+        public int Day { get; set; }
+
+    }
+}
